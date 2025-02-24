bddc017c5ae1b228c45dd74979cad02090501160
Senthilvelan <kssenthilvelan@gmail.com>
2025-02-24 11:50:23 +0530

    java code

---
+++ example/android/app/src/main/java/com/sen/dcmreader/dicom_reader_example/ResponseModel.java
@@ -0,0 +1,64 @@
+package com.sen.dcmreader.dicom_reader_example;
+
+public class ResponseModel {
+    public ResponseModel(
+            byte[] decodedBytes,
+            String patientName,
+            String patientGender,
+            String patientID,
+            String patientAge,
+            String patientBirthDate,
+            String patientWeight,
+            String patientHeight
+    ) {
+        this.decodedBytes = decodedBytes;
+        this.patientName = patientName;
+        this.patientGender = patientGender;
+        this.patientID = patientID;
+        this.patientAge = patientAge;
+        this.patientBirthDate = patientBirthDate;
+        this.patientWeight = patientWeight;
+        this.patientHeight = patientHeight;
+    }
+
+    private final byte[] decodedBytes;
+    private final String patientName;
+    private final String patientGender;
+    private final String patientID;
+    private final String patientAge;
+    private final String patientBirthDate;
+    private final String patientWeight;
+    private final String patientHeight;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public byte[] getDecodedBytes() {
+        return decodedBytes;
+    }
+
+    public String getpatientGender() {
+        return patientGender;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public String getPatientAge() {
+        return patientAge;
+    }
+
+    public String getPatientBirthDate() {
+        return patientBirthDate;
+    }
+
+    public String getPatientWeight() {
+        return patientWeight;
+    }
+
+    public String getPatientHeight() {
+        return patientHeight;
+    }
+}
